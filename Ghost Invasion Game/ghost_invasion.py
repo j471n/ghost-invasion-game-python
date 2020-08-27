2a254d901ef2b5771db40e7ae1909d4ba35943c5
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-08-27 19:16:19 +0530

    Creating a Pygame Window and Responding to User Input

---
+++ Ghost Invasion Game/ghost_invasion.py
@@ -0,0 +1,38 @@
+import sys      #importing sys
+import pygame   #importing pygame
+
+
+class GhostInvasion:
+    #"""Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        #Initialize the game, and create game resources.
+        pygame.init()
+
+        #1200px wide and 800px high is known as surface "display.set_mode() represents the entire game window."
+        self.screen = pygame.display.set_mode((1200, 800))
+        #Caption or title which will display on the top
+        pygame.display.set_caption("Ghost Invasion")
+
+
+#***************************run_game()********************************
+
+#game is controlled by the run_game() method
+    def run_game(self):
+        #"""Start the main loop for the game."""
+
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # It continually updates the display to show the new positions of game
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = GhostInvasion()
+    ai.run_game()
