0832aea32709278eddea6a316902c78689ced4a3
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-08-28 17:58:27 +0530

    Shooting Bullets - Adding the Bullet Settings

--- Ghost Invasion Game/settings.py
+++ Ghost Invasion Game/settings.py
@@ -11,3 +11,9 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 2.0
+
+        # Bullets Settings
+        self.bullet_speed = 3.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)  # 60, 60, 60
