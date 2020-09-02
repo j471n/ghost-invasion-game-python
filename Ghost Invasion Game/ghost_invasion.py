1e2b19f55c7d1ac6375e02ef7a61029f0d3fe8cf
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-02 11:45:15 +0530

    Scoring - Resetting the Score

--- Ghost Invasion Game/ghost_invasion.py
+++ Ghost Invasion Game/ghost_invasion.py
@@ -211,6 +211,7 @@ class GhostInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining ghosts and bullets.
             self.ghosts.empty()
