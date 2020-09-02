fcd169bd73f13196d0909fea432dbba58c348331
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-02 11:48:38 +0530

    Scoring - Making Sure to Score All Hits

--- Ghost Invasion Game/ghost_invasion.py
+++ Ghost Invasion Game/ghost_invasion.py
@@ -275,7 +275,9 @@ class GhostInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.ghosts,True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            
             self.sb.prep_score()
 
         if not self.ghosts:
